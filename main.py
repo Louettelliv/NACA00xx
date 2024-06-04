5c92c28f96b7dc6caac03653df377efaf24a136d
200~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~ <louannevillette2000@gmail.com>
2024-06-04 09:37:46 -0400

    Création du fichier "main.py"

---
+++ main.py
@@ -0,0 +1,8 @@
+"""
+Générateur de Profil Aérodynamique NACA
+Ce programme génère et affiche le profil d'une aile symétrique en utilisant les équations du National Advisory Committee
+for Aeronautics (NACA).
+
+Auteur: Lou-Anne Villette
+Date: 03/06/2024
+"""
